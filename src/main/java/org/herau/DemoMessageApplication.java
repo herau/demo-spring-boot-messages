116f898b8e59cc069f29459b11fec5d8097656c6
Aurélien Leboulanger <aurelien.leboulanger@3ds.com>
2017-03-13 14:41:28 +0100

    Initial commit

---
+++ src/main/java/org/herau/DemoMessageApplication.java
@@ -0,0 +1,20 @@
+package org.herau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class DemoMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMessageApplication.class, args);
+	}
+
+	@GetMapping
+	public String index() {
+		return "index";
+	}
+}
