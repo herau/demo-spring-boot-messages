be906fba480142601bd96f6c22273731c6758076
Aurélien Leboulanger <aurelien.leboulanger@3ds.com>
2017-03-13 14:47:10 +0100

    move messages.properties outside of the project

--- src/main/java/org/herau/DemoMessageApplication.java
+++ src/main/java/org/herau/DemoMessageApplication.java
@@ -2,6 +2,9 @@ package org.herau;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -16,5 +19,12 @@ public class DemoMessageApplication {
 	@GetMapping
 	public String index() {
 		return "index";
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("file:/tmp/messages");
+		return messageSource;
 	}
 }
